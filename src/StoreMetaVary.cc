d50d4c9ccf9dd12be98696a79e879aa7702d1065
teuthida17 <teuthida17@gmail.com>
2016-10-17 12:50:18 +0100

    Squid sources from 3.5.14 release

---
+++ src/StoreMetaVary.cc
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 1996-2016 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+/* DEBUG: section 20    Storage Manager Swapfile Metadata */
+
+#include "squid.h"
+#include "MemObject.h"
+#include "Store.h"
+#include "StoreMetaVary.h"
+
+bool
+StoreMetaVary::checkConsistency(StoreEntry *e) const
+{
+    assert (getType() == STORE_META_VARY_HEADERS);
+
+    if (!e->mem_obj->vary_headers) {
+        /* XXX separate this mutator from the query */
+        /* Assume the object is OK.. remember the vary request headers */
+        e->mem_obj->vary_headers = xstrdup((char *)value);
+        return true;
+    }
+
+    if (strcmp(e->mem_obj->vary_headers, (char *)value) != 0)
+        return false;
+
+    return true;
+}
+
